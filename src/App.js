95ec93f97a62a805652068c917592f8ecb1ff806
deelocruz <kddaniel8000@gmail.com>
2020-01-20 12:18:10 -0500

    user profile update

--- src/App.js
+++ src/App.js
@@ -80,12 +80,27 @@ class App extends React.Component {
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
+    this.setState({ imageUrl: this.state.input });
     app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response =>
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3001/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count}))
+          })
+        }
         this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+      })
       .catch(err => console.log(err));
   }
 
