4716b8da1532c8cd94fab2f0db539021911a201f
deelocruz <kddaniel8000@gmail.com>
2020-01-26 11:04:06 -0500

    commit to heroku

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-      fetch("http://localhost:3001/imageurl", {
+    fetch("https://morning-refuge-35159.herokuapp.com/imageurl", {
         method: "post",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -87,7 +87,7 @@ class App extends React.Component {
       .then(response => response.json())
         .then(response => {
           if (response) {
-            fetch("http://localhost:3001/image", {
+            fetch("https://morning-refuge-35159.herokuapp.com/image", {
               method: "post",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify({
